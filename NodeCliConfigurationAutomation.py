dfc71f2f4870fc1f1ea70a6f689ae1dcdc4733c1
Armagan Saygin <44907803+ArmyNetDevOps@users.noreply.github.com>
2021-08-14 23:11:10 +0300

    Update NodeCliConfigurationAutomation.py

--- NodeCliConfigurationAutomation.py
+++ NodeCliConfigurationAutomation.py
@@ -1,33 +1,41 @@
 import paramiko
 import time
-from getpass import getpass
 import datetime
+TIMENOW = datetime.datetime.now()
 username = input('SSH User : ')
 password = input('SSH Password : ')
-print ('cli Configuration Automation v1.2')
+print ('cli Config Automation v1.2')
 
 DEVICE_IP_LIST = open('DEVICE_IP_LIST.txt')
 for DEVICE in DEVICE_IP_LIST:
     DEVICE = DEVICE.strip()    
-    print ('\n ###### Connecting to the Device ' + DEVICE +'#####\n')
+    print ('\n ###### Connecting to Device ' + DEVICE +'#####\n')
     SSHSESSION = paramiko.SSHClient()
     SSHSESSION.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    SSHSESSION.connect(DEVICE, port=22, username=username, password=password, look_for_keys=False, allow_agent=False)
+    try:
+        SSHSESSION.connect(DEVICE, port=22, username=username, password=password, look_for_keys=False, allow_agent=False,timeout=10)
 
-    DEVICE_ACCESS = SSHSESSION.invoke_shell()
+        DEVICE_ACCESS = SSHSESSION.invoke_shell()
+        
+        DEVICE_ACCESS.send(b'conf t\n')
+        time.sleep(2)
+
+        DEVICE_ACCESS.send(b'do show vlan brief\n')
+        DEVICE_ACCESS.send(b'vlan 2\n')
+        DEVICE_ACCESS.send(b'name 10.10.10.x_Client\n')
+        time.sleep(2)
     
-    DEVICE_ACCESS.send(b'conf t\n')
-    time.sleep(2)
+        OUTPUT = DEVICE_ACCESS.recv(65000)
+        print (OUTPUT.decode('ascii'))
+        #Per Device log - Her node için ayrı log tutar
 
-    DEVICE_ACCESS.send(b'vlan 2\n')
-    DEVICE_ACCESS.send(b'name 10.18.10.x/24_Client\n')
-    time.sleep(2)
-  
-    OUTPUT = DEVICE_ACCESS.recv(65000)
-    print (OUTPUT.decode('ascii'))
-
-    SAVE_DEVICE_OUTPUT = open('NODE_' + DEVICE +('-')+ str(TIMENOW.year)+('-')+
-    str(TIMENOW.month)+('-')+str(TIMENOW.day)+('.txt'), 'w')
-    SAVE_DEVICE_OUTPUT.write(OUTPUT.decode('ascii'))
-    SAVE_DEVICE_OUTPUT.close
-    SSHSESSION.close
+        SAVE_DEVICE_OUTPUT = open('NODE_' + DEVICE +('-')+ str(TIMENOW.year)+('-')+
+        str(TIMENOW.month)+('-')+str(TIMENOW.day)+('.txt'), 'w')
+        SAVE_DEVICE_OUTPUT.write(OUTPUT.decode('ascii'))
+        SAVE_DEVICE_OUTPUT.close
+        SSHSESSION.close
+    except:
+        print("Connection Refused to Node - IP Address : "+DEVICE)
+        Errorlogs = open("ErrorLogs.txt","a")
+        Errorlogs.write("IP Address : "+DEVICE+"\n")
+        Errorlogs.close()
